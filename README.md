719b52e18e07a78a6392a1d1cd5bd4c2e1f45ed6
hxgf <jonathan.youngblood@gmail.com>
2022-05-04 18:01:52 -0500

    ex nihilo

---
+++ README.md
@@ -0,0 +1,91 @@
+# DB Kit
+
+PHP functions to handle database connection and CRUD operations with PDO.
+
+
+## INSTALLATION
+Easy install with composer:
+```
+composer require hxgf/dbkit:0.1.0@dev
+```
+```php
+use DBkit\db;
+require __DIR__ . '/vendor/autoload.php';
+```
+
+## USAGE
+### db::init($settings)
+Initializes the database connection. Should be assigned to a global 'database' variable.
+```php
+$GLOBALS['database'] = db::init([
+  'driver' => 'mysql', // optional, defaults to 'mysql'
+  'host' => 'localhost',
+  'name' => 'database_name',
+  'user' => 'username',
+  'password' => 'password'
+]);
+```
+
+### db::insert($table, $input)
+Sanitizes parameters and inserts an array of data into a specific table. Returns the `id` field of the record created.
+```php
+$new_id = db::insert("celestial_bodies", array(
+  'name' => 'Luna',
+  'classification' => 'moon',
+  'comment' => 'Earth\'s moon, also commonly referred to as "the moon"'
+));
+
+echo $new_id;
+```
+
+### db::find($table, $criteria, $options)
+Sanitizes parameters and retrieves a specific record(/s) from a specific table, building a query with `SELECT *`. Returns an array with the the data and total number of records.
+```php
+$planets = db::find("celestial_bodies", "classification = 'planet' ORDER BY title ASC LIMIT 8");
+
+foreach ($planets['data'] as $p){
+  echo $p['title'];
+  echo $p['classification'];
+  // etc, etc
+}
+
+echo $planets['total'];  // 8
+```
+You can also run a raw query by sending the `raw` parameter like this:
+```php
+$space_objects = db::find("", "SELECT title, classification FROM celestial_bodies WHERE id IS NOT NULL", [
+  'raw' => true
+]);
+```
+\* NOTE: Be careful writing raw queries, as none of the parameters are sanitized. It's recommended to use this with the [db::where_placeholders()](#dbwhere_placeholderscriteria) function below.
+
+
+### db::update($table, $input, $criteria)
+Sanitizes parameters and updates an array of data for a specific record(/s).
+```php
+db::update("celestial_bodies", array(
+  'name' => 'Mars',
+  'comment' => 'Research "The Phobos Incident" -- we are not alone'
+), "name='Marz'");
+```
+
+### db::delete($table, $criteria)
+Sanitizes parameters and deletes a specific record(/s).
+```php
+db::delete("celestial_bodies", "name='venice'");
+```
+
+### db::where_placeholders($criteria)
+Creates placeholders and sanitizes data for query building. This function is used to sanitize parameters for all functions.
+
+Example usage with a raw [db::find()](#dbfindtablecriteriaoptions) query:
+```php
+$wd = db::db_where_placeholders("id='".$_POST['id']."'");
+
+$space_objects = db::find("", "SELECT title, classification FROM celestial_bodies WHERE " . $wd['where'], [
+  'raw' => true
+]);
+```
+
+
+
---
+++ src/db.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace DBkit;
+
+class db {
+  
+	// connect to the database
+	public static function init($args){
+		$dbh = false;
+		if ($args['host']){
+      $args['driver'] = $args['driver'] ? $args['driver'] : 'mysql';
+			try {
+			  $dbh = new PDO($args['driver'].":host=".$args['host'].";dbname=".$args['name'], $args['user'], $args['password']);
+				$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			}
+			catch(PDOException $e) {
+		    echo $e->getMessage();
+			}
+		}
+		return $dbh;
+	}
+
+	// create db placeholders, sanitize data for query building
+	public static function db_where_placeholders($where){
+		$d = preg_match_all('/\'([^\"]*?)\'/', $where, $o);
+		foreach ($o[0] as $ph){
+			$data[] = str_replace("'","",$ph);
+		}
+		$o = array(
+			'where' => preg_replace('/\'([^\"]*?)\'/', '?', $where),
+			'data' => $data,
+		);
+		return $o;
+	}
+
+  // sanitize parameters and retrieve data from mysql, returning array w/ total
+  public static function find($table, $where, $args = false){
+    $qr = false;
+    $wd = db::db_where_placeholders($where);
+    try {
+      $query = "SELECT * FROM $table WHERE " . $wd['where'];
+      if ($args['raw']){
+        $query = $wd['where'];
+      }
+      $a = $GLOBALS['database']->prepare($query);
+      $a->execute($wd['data']);
+      $a->setFetchMode(PDO::FETCH_ASSOC);
+    }
+    catch(PDOException $e) {
+      echo $e->getMessage();
+    }
+    if ($args['cache'] && function_exists("apc_store")){
+      $cache_key = base64_encode($table.$where);
+      if ($qr = apc_fetch($cache_key)){
+      }else{
+        $_args = $args;
+        $_args['cache'] = false;
+        $qr = db_find($table, $where, $_args);
+        $cache_length = 60;
+        if ($args['cache_length']){
+          $cache_length = $args['cache_length'];
+        }
+        apc_store($cache_key, $qr, $cache_length);
+      }
+    }else{
+      $i = 0;
+      while($ad = $a->fetch()){
+        $qr['data'][] = $ad;
+        $i++;
+      }
+      if ($i > 0){
+        $qr['total'] = $i;
+      }
+    }
+    return $qr;
+  }
+
+  // sanitize parameters and insert array of data into mysql, returning the id of the record created
+  public static function insert($table, $input){
+    $columns = '';
+    $placeholders = '';
+    $total = count($input);
+    $i = 1;
+    foreach ($input as $key => $val){
+      $columns .= $key;
+      $placeholders .= ':' . $key;
+      if ($val != NULL){
+        $data[$key] = $val;
+      }else{
+        $data[$key] = NULL;
+      }
+      if ($total != $i){
+        $columns .= ", ";
+        $placeholders .= ", ";
+      }
+      $i++;
+    }
+    try {
+      $a = $GLOBALS['database']->prepare("INSERT INTO $table ($columns) value ($placeholders)");
+      $a->execute($data);
+    }
+    catch(PDOException $e) {
+      echo $e->getMessage();
+    }
+    $o = $GLOBALS['database']->lastInsertId();
+    return $o;
+  }
+
+  // sanitize parameters and update a mysql record
+  public static function update($table, $input, $where){
+    $query = '';
+    $total = count($input);
+    $i = 1;
+    foreach ($input as $key => $val){
+      $query .= $key . ' = ?';
+      if ($val != NULL){
+        $data[] = $val;
+      }else{
+        $data[] = NULL;
+      }
+      if ($total != $i){
+        $query .= ", ";
+      }
+      $i++;
+    }
+    $wd = db::db_where_placeholders($where);
+    foreach ($wd['data'] as $dw){
+      $data[] = $dw;
+    }
+    try {
+      $a = $GLOBALS['database']->prepare("UPDATE $table SET $query WHERE " . $wd['where']);
+      $a->execute($data);
+    }
+    catch(PDOException $e) {
+      echo $e->getMessage();
+    }
+    return true;
+  }
+
+  // sanitize parameters and delete a given mysql record
+  public static function delete($table, $where){
+    $wd = db::db_where_placeholders($where);
+    try {
+      $a = $GLOBALS['database']->prepare("DELETE FROM $table WHERE " . $wd['where']);
+      $a->execute($wd['data']);
+    }
+    catch(PDOException $e) {
+      echo $e->getMessage();
+    }
+    return true;
+  }
+
+}
+
+?>
